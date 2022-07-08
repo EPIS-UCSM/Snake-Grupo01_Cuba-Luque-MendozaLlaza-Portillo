87eced811761719435a945424078749275f554a9
Angie-Cuba <82391844+Angie-Cuba@users.noreply.github.com>
2022-07-08 13:02:10 -0500

    Constructor de la clase Snake
    Inicializamos todos los paremetros

--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -4,6 +4,12 @@ import random
 
 
 class Snake:
-
+    def __init__(self):
+        self.length = 1
+        self.positions = [((screen_width / 2), (screen_height / 2))]
+        self.direction = random.choice([up, down, left, right])
+        self.color = (100, 185, 0)
+        self.score = 0
+        self.highscore = 0
 
 class Food:
