281b477a0ab4b504b0f89484eac097781e669da3
Fabian Portillo Del Carpio <72370904@ucsm.edu.pe>
2022-07-08 14:21:58 -0500

    Se añade a la clase Food un metodo que retone su posicion y otro que la dibuje dentro de la ventaja ejecutada, además se definen atributos que determinaran el ancho y largo de pantalla, las dimensiones de las celdas del fondo y las funciones de arriba, abajo, izquierda y derecha.

--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -72,3 +72,23 @@ class Food:
     def randomize_position(self):
         self.position = (random.randint(0, grid_width - 1) * gridsize, random.randint(0, grid_height - 1) * gridsize)
 
+    def get_food_position(self):
+        return self.position[0]
+
+    def draw(self, surface):
+        r = pygame.Rect((self.position[0], self.position[1]), (gridsize, gridsize))
+        pygame.draw.rect(surface, self.color, r)
+        pygame.draw.rect(surface, (0, 0, 0), r, 1)
+
+screen_width = 480
+screen_height = 480
+
+gridsize = 20
+grid_width = screen_width / gridsize
+grid_height = screen_height / gridsize
+
+up = (0, -1)
+down = (0, 1)
+left = (-1, 0)
+right = (1, 0)
+
